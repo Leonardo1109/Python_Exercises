f3b2a1d40031e50fdefa51dd1165619d35a27ff2
Leonardo1109 <leonardoolveram11@gmail.com>
2025-09-17 21:47:20 -0600

    Commit 17-09_T2_Variables

---
+++ Tareas/T02_Variables.py
@@ -0,0 +1,20 @@
+nombre = "Leonardo"
+edad = 22
+escuela = "Fes Aragon"
+estudiante = True
+
+print(f"""
+       Nombre: {nombre}
+       Edad: {edad}
+       Escuela: {escuela}
+       ¿Estudiante? {estudiante}
+       """)
+
+nombre = "Leonardo Olvera"
+
+print(f"""
+       Nombre: {nombre}
+       Edad: {edad}
+       Escuela: {escuela}
+       ¿Estudiante? {estudiante}
+       """)
