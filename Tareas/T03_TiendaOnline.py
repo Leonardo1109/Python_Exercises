c51b356be50e0f2940db04273b9ca8af2e38a72b
Leonardo1109 <leonardoolveram11@gmail.com>
2025-09-17 21:56:59 -0600

    Commit 17-09_T03_TiendaOnline

---
+++ Tareas/T03_TiendaOnline.py
@@ -0,0 +1,12 @@
+nombreProducto = "Patito24"
+precioProducto = 100
+cantidadInventarioProducto = 56
+estaDisponible = True
+
+print(f"""
+    *** Sistema de Tienda Online ***
+    Producto: {nombreProducto}
+    Precio: {precioProducto}
+    Cantidad inventario: {cantidadInventarioProducto}
+    Disponibilidad: {estaDisponible}
+    """)
